79a2ea2a1a47bf74308e4edcbabc538fb2e4aa54
Emilio Sese <miovsese@gmail.com>
2024-07-26 15:39:31 -0400

    front end changes

--- code/frontend/src/App.js
+++ code/frontend/src/App.js
@@ -1,22 +1,34 @@
-import logo from './logo.svg';
+import React from 'react';
 import './App.css';
+import logo from './tripleD_logo.png';
 
 function App() {
+  const handleCustomerClick = () => {
+    alert('Customer button clicked');
+  };
+
+  const handleStaffClick = () => {
+    alert('Staff button clicked');
+  };
+
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <div className="container-header">
+          <h1>
+            <span className="triple">triple</span>
+            <span className="d-shop">D</span>
+          </h1>
+          <img src={logo} className="App-logo" alt="logo" />
+        </div>
+        <div className="button-container">
+          <button className="button customer-button" onClick={handleCustomerClick}>
+            Customer
+          </button>
+          <button className="button staff-button" onClick={handleStaffClick}>
+            Staff
+          </button>
+        </div>
       </header>
     </div>
   );
